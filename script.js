2c69599e4a76bbbd547fa2aceb7ee77a1c94e012
jkarthikarav <karthikaravindj@gmail.com>
2025-04-29 18:11:02 -0400

    cursor updates

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function closeModal() {
   // Hide the modal
   modal.style.display = "none";
 }
+
+
+// Create the floating circle
+const cursorCircle = document.createElement('div');
+cursorCircle.classList.add('custom-cursor');
+document.body.appendChild(cursorCircle);
+
+// Instantly follow mouse
+document.addEventListener('mousemove', (e) => {
+  cursorCircle.style.top = e.clientY + 'px';
+  cursorCircle.style.left = e.clientX + 'px';
+});
+
+// Fade out the circle over text
+const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, a, .tag');
+textElements.forEach(el => {
+  el.addEventListener('mouseenter', () => {
+    cursorCircle.style.opacity = '0';
+  });
+  el.addEventListener('mouseleave', () => {
+    cursorCircle.style.opacity = '1';
+  });
+});
