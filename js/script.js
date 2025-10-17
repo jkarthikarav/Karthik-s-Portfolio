f38878dc6d9fe495b60c86e81dab0162f4c4436c
jkarthikarav <karthikaravindj@gmail.com>
2025-10-16 22:43:45 -0400

    reorg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-  fetch('addons/header.html')
+  fetch('../../addons/header.html')
     .then(response => {
       if (!response.ok) throw new Error('Failed to load header');
       return response.text();
